3ce2e9efdb1f402c98cb4d256650e47df136926d
iamBedant <bedanta.borah@kuliza.com>
2017-03-02 11:52:26 +0530

    Basic Calc working

---
+++ android/app/src/main/java/com/dictionary/LauncherActivity.java
@@ -0,0 +1,27 @@
+package com.dictionary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LauncherActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_launcher);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        Intent intent = new Intent(LauncherActivity.this,MainActivity.class);
+        startActivity(intent);
+      }
+    });
+  }
+}
